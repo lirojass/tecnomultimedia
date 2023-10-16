637f64d9363b4fdad1c9e4a257b5b8a00ccae818
lirojass <128868350+lirojass@users.noreply.github.com>
2023-10-16 20:39:38 -0300

    Add files via upload

---
+++ imagenes.js
@@ -0,0 +1,12 @@
+let imagenes = [];
+
+function preload(){
+  imagenes[1]=[]
+imagenes[1][0]= loadImage("data/piedraTP5.png");
+imagenes[1][1]= loadImage("data/piedraTP51.png");
+imagenes[1][2]= loadImage("data/ZanahoriaTP5.png");
+imagenes[1][3]= loadImage("data/ZanahoriaIconoTP5.png");
+imagenes[1][4]= loadImage("data/trigoTP5.png");
+imagenes[1][5]= loadImage("data/trigoIconoTP5.png");
+imagenes[1][6]= loadImage("data/fondoTP5.png");
+}
