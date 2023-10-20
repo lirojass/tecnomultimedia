d98da3840031c94331e6e7c8e1ad37b0c8806f79
lirojass <128868350+lirojass@users.noreply.github.com>
2023-10-20 20:38:31 -0300

    Update HUD.js

--- HUD.js
+++ HUD.js
@@ -10,38 +10,40 @@ class HUD {
     this.yTiempo=25;
     this.tamañoTexto=25;
     this.tiempo=tiempo;
+    this.frames=0;
   }
 
-  dibujar(trigo,zanahorias,tiempo) {
+  dibujar(trigo, zanahorias, tiempo) {
     for (let i=0; i<this.cantidadVidas; i++) {
-      push();
-      noStroke();
-      fill(255, 0, 0);
-      rect(this.xVidas+this.anchoVidas*i+25*i, this.yVidas, this.anchoVidas, this.alturaVidas)
-        pop();
+      image(imagenes[2][this.frames],this.xVidas+this.anchoVidas*i+25*i, this.yVidas, this.anchoVidas, this.alturaVidas);
     }
     fill(255);
-    textAlign(CENTER,CENTER)
+    textAlign(CENTER, CENTER);
     textSize(48);
-    text(tiempo,width/2,50);
-    this.puntaje(trigo,zanahorias)
+    text(tiempo, width/2, 50);
+    this.puntaje(trigo, zanahorias)
   }
-  
-  puntaje(trigo,zanahorias){
-  push();
-  noStroke();
-  textAlign(LEFT,CENTER);
-  textSize(24);
-  image(imagenes[1][5],550,25,50,50);
-  image(imagenes[1][3],675,25,50,50);
-  fill(255);
-  text(trigo,620,50);
-  text(zanahorias,745,50);
-  pop();
+
+  puntaje(trigo, zanahorias) {
+    push();
+    noStroke();
+    textAlign(LEFT, CENTER);
+    textSize(24);
+    image(imagenes[1][5], 550, 25, 50, 50);
+    image(imagenes[1][3], 675, 25, 50, 50);
+    fill(255);
+    text(trigo, 620, 50);
+    text(zanahorias, 745, 50);
+    pop();
   }
-  
-  actualizar(nuevo){
-  this.cantidadVidas=nuevo;
+
+  actualizar(nuevo) {
+    this.cantidadVidas=nuevo;
+    if(frameCount%10==0){
+    this.frames++;
+    }
+    if (this.frames>5) {
+      this.frames=0;
+    }
   }
-  
 }
