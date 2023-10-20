b7afe604992e7c109b0c6d27f3c651af5dc84718
lirojass <128868350+lirojass@users.noreply.github.com>
2023-10-20 20:38:14 -0300

    Update JuegoCompleto.js

--- JuegoCompleto.js
+++ JuegoCompleto.js
@@ -4,12 +4,22 @@ class TP5 {
     textSize(24);
     textAlign(CENTER, CENTER);
     this.Juego1=new juego();
+    this.botonInicio= new boton(width/2-100, 600, "Iniciar");
+    this.botonFinal= new boton(width/2-100, 600, "Reiniciar");
     this.pausa=false;
-    this.escenario=0;
+    this.escenario=-1;
   }
 
   dibujar() {
     switch(this.escenario) {
+      case-1:
+      push();
+      background(0);
+      fill(255);
+      text("Tu objetivo es que mientras te muevas tienes que agarrar, a tu elección, trigo y/o zanahorias, mientras esquivas piedras en el camino, te puedes mover de lado a lado con las flechas del teclado",100,100,600,600);
+      this.botonInicio.dibujar();
+      pop();
+      break;
     case 0:
       this.Juego1.dibujar()
         if (this.pausa) {
@@ -22,24 +32,40 @@ class TP5 {
       }
       break;
     case 1:
-      push();
-      background(200, 0, 0);
-      fill(255);
-      text("Perdiste", width/2, height/2);
-      pop();
+      image(imagenes[3][0], 0, 0, width, height);
+      this.Juego1.reiniciar();
+      this.botonFinal.dibujar();
       break;
     case 2:
-      push();
-      background(0, 0, 200);
-      fill(255);
-      text("Final", width/2, height/2);
-      pop();
+      if (this.Juego1.DevolverPuntajeTrigo()==0&&this.Juego1.DevolverPuntajeZanahoria()==0) {
+        push();
+        background(200, 0, 0);
+        fill(255);
+        text("Perdiste", width/2, height/2);
+        pop();
+      } else {
+        let resultado =this.Juego1.DevolverPuntajeTrigo()-this.Juego1.DevolverPuntajeZanahoria();
+        if (resultado<-20) {
+          image(imagenes[3][1], 0, 0, width, height);
+        } else if (resultado<0) {
+          image(imagenes[3][2], 0, 0, width, height);
+        } else if (resultado>20) {
+          image(imagenes[3][4], 0, 0, width, height);
+        } else if (resultado>=0) {
+          image(imagenes[3][3], 0, 0, width, height);
+        }
+        this.Juego1.reiniciar();
+        this.botonFinal.dibujar();
+      }
       break;
     }
   }
 
   actualizar() {
     switch(this.escenario) {
+      case-1:
+      this.botonInicio.actualizar();
+      break;
     case 0:
       if (!this.pausa) {
         this.Juego1.actualizar()
@@ -51,17 +77,46 @@ class TP5 {
         this.escenario=2;
       }
       break;
+    case 1:
+      this.botonFinal.actualizar();
+      break;
+    case 2:
+      this.botonFinal.actualizar();
+      break;
     }
   }
 
   tecladoApretado() {
-    this.Juego1.keyApretado()
-      if (keyCode==ESCAPE) {
-      this.pausa=!this.pausa;
+    switch(this.escenario) {
+    case 0:
+      this.Juego1.keyApretado()
+        if (keyCode==ESCAPE) {
+        this.pausa=!this.pausa;
+      }
+      break;
     }
   }
-
   tecladoLibre() {
     this.Juego1.keyLibre()
+  }
+
+  mouseApretado() {
+    switch(this.escenario) {
+      case-1:
+      if(this.botonInicio.mouse()){
+      this.escenario=0;
+      }
+      break;
+    case 1:
+      if(this.botonFinal.mouse()){
+      this.escenario=0;
+      }
+      break;
+    case 2:
+      if(this.botonFinal.mouse()){
+      this.escenario=0;
+      }
+      break;
+    }
   }
 }
