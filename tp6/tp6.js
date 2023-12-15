a825dd53b4019dc6607fcd784a96bf6575384ba8
lirojass <128868350+lirojass@users.noreply.github.com>
2023-12-14 22:40:53 -0300

    Add files via upload

---
+++ tp6/tp6.js
@@ -0,0 +1,27 @@
+let tp6;
+
+function setup() {
+  createCanvas(800, 800);
+  //let canvas = createCanvas(800, 800);
+  //canvas.position(windowWidth/2-width/2, windowHeight/2-height/2);
+  textFont("Merriweather");
+  tp6= new GatoConBotas();
+}
+
+
+function draw() {
+tp6.dibujar(imagen,script);
+tp6.actualizar();
+}
+
+function mousePressed(){
+  tp6.mouse();
+}
+
+function keyPressed(){
+  tp6.keyPresion();
+}
+
+function keyReleased(){
+  tp6.keyLibre();
+}
